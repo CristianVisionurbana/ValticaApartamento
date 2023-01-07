db04750f055f062223a507cb500965300ba5dc19
CristianVisionurbana <122174567+CristianVisionurbana@users.noreply.github.com>
2023-01-07 12:20:49 -0600

    Add files via upload

---
+++ layama.scene.js
@@ -0,0 +1,50 @@
+// Created with Motiva Layama v1.6 https://www.motivacg.com/layama
+
+function getLayamaCameras()
+{
+   var layamaCameras = new BABYLON.SmartArray(0);
+   layamaCameras.push({n: "Departamento Valtica0000", a: "PhysCamera001", p: new BABYLON.Vector3(707.457, 1056.98, 4848.84), l: new BABYLON.Vector3(707.457, 1056.98, 4847.84)});
+   layamaCameras.push({n: "Departamento Valtica0001", a: "PhysCamera002", p: new BABYLON.Vector3(570.795, 1056.98, 4848.84), l: new BABYLON.Vector3(570.795, 1056.98, 4847.84)});
+   layamaCameras.push({n: "Departamento Valtica0002", a: "PhysCamera003", p: new BABYLON.Vector3(711.685, 1056.98, 4703.14), l: new BABYLON.Vector3(711.685, 1056.98, 4702.14)});
+   layamaCameras.push({n: "Departamento Valtica0003", a: "PhysCamera004", p: new BABYLON.Vector3(711.685, 1056.98, 4570.58), l: new BABYLON.Vector3(711.685, 1056.98, 4569.58)});
+   layamaCameras.push({n: "Departamento Valtica0004", a: "PhysCamera005", p: new BABYLON.Vector3(711.685, 1056.98, 4351.32), l: new BABYLON.Vector3(711.685, 1056.98, 4350.32)});
+   layamaCameras.push({n: "Departamento Valtica0005", a: "PhysCamera006", p: new BABYLON.Vector3(497.637, 1056.98, 4620.5), l: new BABYLON.Vector3(497.637, 1056.98, 4619.5)});
+   layamaCameras.push({n: "Departamento Valtica0006", a: "PhysCamera007", p: new BABYLON.Vector3(497.637, 1056.98, 4380.49), l: new BABYLON.Vector3(497.637, 1056.98, 4379.49)});
+   layamaCameras.push({n: "Departamento Valtica0007", a: "PhysCamera008", p: new BABYLON.Vector3(427.632, 1056.98, 4484.15), l: new BABYLON.Vector3(427.632, 1056.98, 4483.15)});
+   layamaCameras.push({n: "Departamento Valtica0008", a: "PhysCamera009", p: new BABYLON.Vector3(893.391, 1056.98, 4770.09), l: new BABYLON.Vector3(893.391, 1056.98, 4769.09)});
+   layamaCameras.push({n: "Departamento Valtica0009", a: "PhysCamera010", p: new BABYLON.Vector3(1078.52, 1056.98, 4770.09), l: new BABYLON.Vector3(1078.52, 1056.98, 4769.09)});
+   layamaCameras.push({n: "Departamento Valtica0010", a: "PhysCamera011", p: new BABYLON.Vector3(1105.36, 1056.98, 4607.39), l: new BABYLON.Vector3(1105.36, 1056.98, 4606.39)});
+   layamaCameras.push({n: "Departamento Valtica0011", a: "PhysCamera012", p: new BABYLON.Vector3(912.904, 1056.98, 4594.87), l: new BABYLON.Vector3(912.904, 1056.98, 4593.87)});
+   layamaCameras.push({n: "Departamento Valtica0012", a: "PhysCamera013", p: new BABYLON.Vector3(1281.27, 1056.98, 4614.42), l: new BABYLON.Vector3(1281.27, 1056.98, 4613.42)});
+   layamaCameras.push({n: "Departamento Valtica0013", a: "PhysCamera014", p: new BABYLON.Vector3(1417.86, 1056.98, 4614.42), l: new BABYLON.Vector3(1417.86, 1056.98, 4613.42)});
+   layamaCameras.push({n: "Departamento Valtica0014", a: "PhysCamera015", p: new BABYLON.Vector3(1417.86, 1056.98, 4813.92), l: new BABYLON.Vector3(1417.86, 1056.98, 4812.92)});
+   layamaCameras.push({n: "Departamento Valtica0015", a: "PhysCamera016", p: new BABYLON.Vector3(1078.63, 1056.98, 4346.85), l: new BABYLON.Vector3(1078.63, 1056.98, 4345.85)});
+   layamaCameras.push({n: "Departamento Valtica0016", a: "PhysCamera017", p: new BABYLON.Vector3(907.602, 1056.98, 4346.85), l: new BABYLON.Vector3(907.602, 1056.98, 4345.85)});
+   return layamaCameras;
+}
+
+function getLayamaResolutions()
+{
+   var layamaResolutions = new BABYLON.SmartArray(0);
+   layamaResolutions.push("2048");
+   layamaResolutions.push("1024");
+   return layamaResolutions;
+}
+
+function getOnScreenLogoUsage()
+{
+   return 6;
+}
+
+function getLayamaControls()
+{
+   return {defMove: false, defRot: 1, altMove: true, altRot: 2};
+}
+
+function getLayamaHotspots()
+{
+   var layamaHotspots = new BABYLON.SmartArray(0);
+   layamaHotspots.push({n: "LayamaHotspot001", m: 1, u: "https://www.youtube.com/embed/xFnsMUAMpsE?autoplay=1", i: "./hotspots/Play.png", p: new BABYLON.Vector3(369.316, 1049.11, 4457.28), d: 220});
+   return layamaHotspots;
+}
+
